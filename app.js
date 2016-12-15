5c5468fb1081369a80398f3b2fe590d9bdb503ca
Carl Blundell <carl.blundell@gmail.com>
2016-12-15 12:19:09 +0000

    Add index.html

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 var restify = require('restify')
 var builder = require('botbuilder')
 
+
 // Set up the restify server
 // 'restify is a node.js module built specifically to enable you to build correct REST web services'
 // server created and given a callback function
@@ -14,19 +15,24 @@ server.listen(process.env.PORT || 3000, function()
    console.log('%s listening to %s', server.name, server.url); 
 });
 
-// Get my app environment vars
-var myAppId = process.env.MY_APP_ID,
-var myAppPassword = process.env.MY_APP_PASSWORD;
+
 
 // Create chat bot
 // The ChatConnector 'Connects a UniversalBot to multiple channels via the Bot Framework'
 // more at https://docs.botframework.com/en-us/node/builder/chat-reference/classes/_botbuilder_d_.chatconnector.html
 var connector = new builder.ChatConnector
-({ appId: myAppId, appPassword: myAppPassword }); 
+({ appId: process.env.MY_APP_ID, appPassword: process.env.MY_APP_PASSWORD }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
+server.get('/', restify.serveStatic({
+ directory: __dirname,
+ default: '/index.html'
+}));
+
 // Create bot dialogs
 bot.dialog('/', function (session) {
-    session.send("Hello World");
+    var retMessage="Hello World"
+    console.log(retMessage);
+    session.send(retMessage);
 });
