99b2e327f8d6ea82c3dfb6465303edfafcc185b2
Carl Blundell <carl.blundell@gmail.com>
2016-12-15 13:57:39 +0000

    Website restored in code

--- app.js
+++ app.js
@@ -24,12 +24,10 @@ var connector = new builder.ChatConnector
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-/*
 server.get('/', restify.serveStatic({
  directory: __dirname,
  default: '/index.html'
 }));
-*/
 
 // Create bot dialogs
 bot.dialog('/', function (session) {
