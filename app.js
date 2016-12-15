3de0ca9a2879ee578dbe60dda9435080493f9c29
Carl Blundell <carl.blundell@gmail.com>
2016-12-15 16:24:40 +0000

    Basic name handling stage 1

--- app.js
+++ app.js
@@ -40,8 +40,26 @@ server.get('/', restify.serveStatic({
 }));
 
 // Create bot dialogs
-bot.dialog('/', function (session) {
-    var retMessage="Hello World"
-    console.log(retMessage);
-    session.send(retMessage);
-});
+bot.dialog('/', [
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
